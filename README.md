c2590ec5f1c6efd4361867924e62d6ecf9930f26
Matthieu Pignolet <matthieu@matthieu-dev.xyz>
2024-12-24 16:47:33 +0400

    minor packaging changes

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Ankama Zaap Flatpak
+
+This is a community packaging of the Zaap Launcher (Also known as Dofus Launcher or Ankama Launcher) as a FlatPak
+
+> This is a community Flatpak packaging not endorsed by Ankama.
--- apply_extra.sh
+++ apply_extra.sh
@@ -2,20 +2,12 @@
 
 APP_IMAGE="/app/extra/Zaap.AppImage"
 
-# Allow image to execute
-
 chmod +x $APP_IMAGE
-
-# Extract image
-
 unappimage $APP_IMAGE
-
-# Install data
 
 DEST="/app/extra/bin/"
 mkdir $DEST
 cp -r squashfs-root/* $DEST
 
-# Clean up
 rm -rf squashfs-root/
 rm $APP_IMAGE
